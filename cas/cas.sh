e8044c5f345f5e63e31aee1cbe4af4f0ed45fc6f
Cas Eliëns <cas.eliens@gmail.com>
2018-11-23 21:34:24 +0100

    Update cas.sh

--- cas/cas.sh
+++ cas/cas.sh
@@ -1,7 +1,34 @@
+# Configure authorized SSH keys
 cd ~/.ssh; cp authorized_keys temp.txt; curl -s https://github.com/cascer1.keys >> temp.txt; awk '!seen[$0] ++' temp.txt > authorized_keys
 
+# Load bash and vim preferences
 bash <(curl -s https://raw.githubusercontent.com/cascer1/dotbash/install.sh)
 bash <(curl -s https://raw.githubusercontent.com/cascer1/dotvim/install.sh)
 
+# Gist tool for easy log uploading
 apt-get install -y git vim ethtool ruby2.3
 gem install gist
+
+# Ask for ngrok and ssh info
+read -p "What SSH port would you like to use? " -r
+sshport=$REPLY
+
+read -p "What is your ngrok hostname? " -r
+ngrokhost=$REPLY
+
+read -p "What is your ngrok access token? " -r
+ngroktoken=$REPLY
+
+# Move required config files into place
+cp ~/src/oref0/cas/ngrok.conf /usr/local/etc/ngrok.conf
+cp ~/src/oref0/cas/ngrok.service /etc/systemd/system/ngrok.service
+
+# Replace placeholders with actual data in config files
+sed -i -r "s/#*\s*Port .*/Port $sshport/g" /etc/ssh/sshd_config
+sed -i -r "s/#*\s*PermitRootLogin .*/PermitRootLogin prohibit-password/g" /etc/ssh/sshd_config
+
+sed -i -r 's/##AUTHTOKEN##/$ngroktoken/g' /usr/local/etc/ngrok.conf
+sed -i -r 's/##HOSTNAME##/$ngrokhost/g' /usr/local/etc/ngrok.conf
+sed -i -r 's/##SSHPORT##/$sshport/g' /usr/local/etc/ngrok.conf
+
+echo "service ngrok restart" >> /etc/rc.local
